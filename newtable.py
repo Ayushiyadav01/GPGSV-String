0b61106c62a61057c5694b45c579e54dc58ea5d8
Ayushiyadav01 <91016502+Ayushiyadav01@users.noreply.github.com>
2022-06-29 15:35:08 +0530

    live data update - scrolling data live

--- newtable.py
+++ newtable.py
@@ -1,6 +1,11 @@
+from cProfile import label
 from tkinter import *
 from  tkinter import ttk
+from time import process_time_ns
+from turtle import goto
+import serial
 
+ser = serial.Serial('COM5', 4800, timeout=0)
 
 ws  = Tk()
 ws.title('GPGSV')
@@ -12,31 +17,68 @@ game_frame.pack()
 
 my_game = ttk.Treeview(game_frame)
 
-my_game['columns'] = ('player_id', 'player_name', 'player_Rank')
+my_game['columns'] = ('Sat_No', 'Elev_Angle', 'Azim_Angle')
 
 my_game.column("#0", width=0,  stretch=NO)
-my_game.column("player_id",anchor=CENTER, width=150)
-my_game.column("player_name",anchor=CENTER,width=150)
-my_game.column("player_Rank",anchor=CENTER,width=150)
+my_game.column("Sat_No",anchor=CENTER, width=150)
+my_game.column("Elev_Angle",anchor=CENTER,width=150)
+my_game.column("Azim_Angle",anchor=CENTER,width=150)
 
 my_game.heading("#0",text="",anchor=CENTER)
-my_game.heading("player_id",text="Satellite Number",anchor=CENTER)
-my_game.heading("player_name",text="Elevation Angle",anchor=CENTER)
-my_game.heading("player_Rank",text="Azimuth Angle",anchor=CENTER)
+my_game.heading("Sat_No",text="Satellite Number",anchor=CENTER)
+my_game.heading("Elev_Angle",text="Elevation Angle",anchor=CENTER)
+my_game.heading("Azim_Angle",text="Azimuth Angle",anchor=CENTER)
 
-my_game.insert(parent='',index='end',iid=0,text='',
-values=('1','Ninja','101'))
-my_game.insert(parent='',index='end',iid=1,text='',
-values=('2','Ranger','102'))
-my_game.insert(parent='',index='end',iid=2,text='',
-values=('3','Deamon','103'))
-my_game.insert(parent='',index='end',iid=3,text='',
-values=('4','Dragon','104'))
-my_game.insert(parent='',index='end',iid=4,text='',
-values=('5','CrissCross','105'))
-my_game.insert(parent='',index='end',iid=5,text='',
-values=('6','ZaqueriBlack','106'))
+def data():
+    while True :
+        data_raw = str(ser.readline())
+        data_li = list(data_raw.split(","))
+        # print(data_li)
+        if (data_li[0]=='b\'$GPGSV'):
+            try:
+                sat_no1 = data_li[4]
+                elev1 = data_li[5]
+                azi1 = data_li[6]
+            except:
+                print("data over1")
+
+            try:
+                sat_no2 = data_li[8]
+                elev2 = data_li[9]
+                azi2 = data_li[10]
+            except:
+                print("Data over2")
+
+            try:                    
+                sat_no3 = data_li[12]
+                elev3 = data_li[13]
+                azi3 = data_li[14]
+            except:
+                print("data over3")
+
+            try:
+                sat_no4 = data_li[16]
+                elev4 = data_li[17]
+                azi4 = data_li[18] 
+            except:
+                print("data over4")
+
+            my_game.insert(parent='',index='end',text='',
+            values=(sat_no1,elev1,azi1))
+            my_game.insert(parent='',index='end',text='',
+            values=(sat_no2,elev2,azi2))
+            my_game.insert(parent='',index='end',text='',
+            values=(sat_no3,elev3,azi3))
+            my_game.insert(parent='',index='end',text='',
+            values=(sat_no4,elev4,azi4))
+           
+            ws.update()
+                
 
 my_game.pack()
+
+bu = Button(ws,text=("Start"),command=data)
+bu.pack()
+
 
 ws.mainloop()
